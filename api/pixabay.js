1e7378a9faec0ad378fa8804d1e93e07e120dc19
Cocobean98 <47190059+Cocobean98@users.noreply.github.com>
2026-06-08 21:32:56 +0300

    Add files via upload

---
+++ api/pixabay.js
@@ -0,0 +1,26 @@
+{
+  "total": 1234,
+  "totalHits": 500,
+  "hits": [
+    {
+      "id": 12345,
+      "pageURL": "...",
+      "type": "film",
+      "tags": "nature, forest",
+      "duration": 15,
+      "picture_id": "...",
+      "videos": {
+        "large": { "url": "...", "width": 1920, "height": 1080 },
+        "medium": { "url": "...", "width": 1280, "height": 720 },
+        "small": { "url": "...", "width": 960, "height": 540 }
+      },
+      "views": 1000,
+      "downloads": 500,
+      "likes": 100,
+      "comments": 10,
+      "user_id": 123,
+      "user": "username",
+      "userImageURL": "..."
+    }
+  ]
+}
